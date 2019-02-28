c9a3406bd09ca594b72d6120aa380f8049732f2c
Jameka Echols <jameka.echols@gmail.com>
2019-02-28 13:10:33 -0600

    finished practice

---
+++ main.js
@@ -0,0 +1,55 @@
+// This exercise does not require you to alter 
+// this function. But read it. See if you can follow what it takes in, executes, and returns.
+
+
+const hamburger = {
+	name: 'Hamburger',
+	type: 'beef',
+	cooked: false,
+}
+const zucchini = {
+	name: 'Zucchini',
+	type: 'vegetable',
+	cooked: false,
+}
+const chickenBreast = {
+	name: 'Chicken Breast',
+	type: 'chicken',
+	cooked: false,
+}
+const corn = {
+	name: 'Corn',
+	type: 'vegetable',
+	cooked: false,
+}
+const steak = {
+	name: 'Steak',
+	type: 'beef',
+	cooked: false,
+}
+
+// An array that is grouping the objects together.
+const foods = [hamburger, zucchini, chickenBreast, corn, steak];
+
+// An empty array that will store the objects after the `grill()` function cooks the food.
+const cookedFood = [];
+
+// Your task is to iterate over the array of foods and invoke the function 
+// for each item so that the cookedFood array contains all of the items after they are cooked.
+
+function grill (currentObject) {
+    // Modify the food so that it is cooked
+    currentObject.cooked = true
+    cookedFood.push(currentObject);
+    // console.log(currentObject.name);
+
+};
+
+for (let i = 0; i < foods.length; i++) {
+    grill(foods[i]);
+    //console.log(foods[i].name);
+}
+
+// for (let i =0; i < cookedFood.length; i++){
+//     console.log(cookedFood[i].name);
+// }
